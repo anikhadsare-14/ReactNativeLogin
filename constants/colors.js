b75c7a5aa52f6662a6fba7189f23594f7d0716bf
animesh.k <animesh.khadsare14@gmail.com>
2024-06-23 14:03:00 +0530

    colros added

---
+++ constants/colors.js
@@ -0,0 +1,14 @@
+export default {
+    white : '#fff',
+    text : '#343434',
+    borderColor : '#e2e2e2',
+    blue : '#197ee3',
+    textLight : '#797979',
+    offWhite: "#F0F0F0",
+    _e8ecf4 : '#e8ecf4',
+    _1e1e1e : '#1e1e1e',
+    _929292 : '#929292',
+    _222 : "#222",
+    _6b7288 : "#6b7288",
+    _075ccc : '#075ccc'
+};
